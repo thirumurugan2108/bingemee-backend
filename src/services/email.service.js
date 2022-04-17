c9c5b06ab6ba10f94092218d7cbba01409002f93
vijaykarthick <p.vijaykarthick@gmail.com>
2022-04-17 13:25:37 +0530

    verifyotp fix

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -39,14 +39,20 @@ If you did not request any password resets, then ignore this email.`;
   await sendEmail(to, subject, text);
 };
 
-const sendOTP = async (to, name) => {
+const sendOTP = async (to, name, influencer = '') => {
   const otp = Math.floor(100000 + Math.random() * 900000);
   const subject  = 'Here\'s the authorization code you asked for'
   const text = `Dear ${name},
    To verify your email, please use the below OTP with in 10 mins.
    ${otp}
+   or use below link to http://localhost:3000/influencer/${influencer}?validateEmail=true&email=${to} enter the otp
    `;
-  await sendEmail(to, subject, text);
+  const html = `Dear ${name},
+  To verify your email, please use the below OTP with in 10 mins.
+  <b>${otp}</b> 
+  or <a href="http://localhost:3000/influencer/${influencer}?validateEmail=true&email=${to}">click here to enter the OTP</a>
+  ` 
+  await sendEmail(to, subject, text, html);
   return otp
 }
 
