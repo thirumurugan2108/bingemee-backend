cf7126284a4b3b2651dc6717ef113480f4aabe3e
vijaykarthick <p.vijaykarthick@gmail.com>
2022-04-20 18:21:34 +0530

    file limit issue

--- src/app.js
+++ src/app.js
@@ -32,9 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({limit: '50mb', extended: true }));
 
 app.use(express.json({limit: '50mb'}));
-
-// app.use(bodyParser.json({limit: '50mb'}));
-// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 // sanitize request data
 app.use(xss());
 app.use(mongoSanitize());
