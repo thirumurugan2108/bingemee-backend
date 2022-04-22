5444a13067781b6b62506b39975a577c754c7dce
thirumurugan2108 <thirumurugan2108@gmail.com>
2022-04-22 23:38:59 +0530

    delete card

--- src/routes/v1/card.route.js
+++ src/routes/v1/card.route.js
@@ -9,6 +9,10 @@ router
   .post(auth('createCard'),  cardController.createCard)
   .get(auth('createCard'),cardController.getCard)
   .patch(auth('updateCard'), cardController.updateCardStatus)
+  // .delete(auth('deleteCard'), cardController.deleteCardById)
 
+  router
+  .route('/deleteCard/')
+  .post(auth('deleteCard'),cardController.deleteCardById);
 
 module.exports = router;
