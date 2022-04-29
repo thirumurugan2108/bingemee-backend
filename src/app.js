f4474171121fa11cd59695aff4c5fe00f2884af5
vijaykarthick <p.vijaykarthick@gmail.com>
2022-04-29 23:04:54 +0530

    fixing cors

--- src/app.js
+++ src/app.js
@@ -42,11 +42,22 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-app.use(cors());
+app.use(cors({
+  origin: ['https://bingemeee.com', 'https://www.bingemeee.com', 'https://api.bingemeee.com']
+}));
 app.options('*', cors({
-  origin: 'https://bingemeee.com'
+  origin: ['https://bingemeee.com', 'https://www.bingemeee.com', 'https://api.bingemeee.com']
 }));
 
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://bingemeee.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 // jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
